8f268ca0a3eee177a1000e05d64aa9d908e58ba1
Ronny Cabrera <ronny.cabrera@ec.pe>
2015-02-05 14:58:30 -0500

    feat(demojs): agregando operaciones con el objeto math

--- numeros.js
+++ numeros.js
@@ -4,7 +4,21 @@ console.log('2 * 2' + ' = '+ 2 * 2);
 console.log('11 - 9' + ' = '+ (11 - 9));
 console.log('9 - 11' + ' = '+ (9 - 11));
 console.log('101.5 - 14.2'+' = '+ (101.5 - 14.2));
-console.log('110 / 4'+' = '+ (110 / 4));
+console.log('110 / 4'+' = '+ 110 / 4);
+console.log('310 / 15.5'+' = '+ 310 / 15.5);
+console.log('55 % 20'+' = '+ 55 % 20);
+console.log('sacando el 30 % de 80'+' = '+ 80 * 30 / 100);
+console.log('-----------------usando Objeto Math-----------------');
+console.log('valor de PI es: '+ Math.PI);
+console.log('valor de PI * 6 es: '+Math.PI * 6);
+console.log(Math.E);
+console.log('2 elevado a 7 es: '+Math.pow(2, 7));
+console.log('La tangente de 180 es: '+Math.tan(180));
+
+
+
+
+
 
 
 
