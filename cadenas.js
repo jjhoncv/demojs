d8c017f5b8965c5495e9c969be02add569348d03
Ronny Cabrera <ronny.cabrera@ec.pe>
2015-02-05 17:28:30 -0500

    feat(demojs): ahora nos vamos con cadenas, variables y arrays

--- cadenas.js
+++ cadenas.js
@@ -1,1 +1,5 @@
-cadenas.js
+console.log('---------------------------Empezamos con las cadenas-------------------------');
+console.log('Usamos length para contar los caracteres de la siguiente oracion "Pablito clavo un clavito" es: '+("Pablito clavo un clavito").length);
+console.log('En esta linea las palabras seran mayusculas '+'Esta oracion se convirtio a mayusculas'.toUpperCase());
+console.log('Separando caracteres con el metodo split'.split(""));
+console.log('---------------------------Termine con cadenas y ahora nos vamos con las variables-------------------------');
---
+++ variables.js
@@ -0,0 +1,16 @@
+var thisIsAVariable = "esta nota es una variable llamada (thisIsAVariable)";
+console.log(thisIsAVariable);
+console.log('--------------aqui nomas empezamos con los Arrays---------------------');
+var numbers = [3, 4, 7, 14, 34, 53];
+console.log('Escribimos el total  de los elementos del array llamado numbers '+numbers.length);
+var date = [5, 2, 2015];
+console.log('esto es el array "date" ' + date);
+console.log('Y ahora lo convertimos en fecha usando el metodo join ' + date.join('/')+ ' se leeria 5 de febrero del 2015');
+var aunt = 'mi tia Lorenita';
+	list = ["papa", "cebolla", "arroz", "pollo"];
+console.log('La lista de '+ aunt + ' contiene ' + list.join(', ')+  ' y aji.');
+console.log(new Object(false));
+
+
+
+
