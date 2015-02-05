47fc1f69b1758c86b7fe61303b700bfeccac389b
Ronny Cabrera <ronny.cabrera@ec.pe>
2015-02-05 15:50:05 -0500

    feat(demojs): agregando operaciones propias

--- numeros.js
+++ numeros.js
@@ -1,12 +1,22 @@
 console.log('1 + 10' + ' = '+ (1 + 10));
-console.log('0.5 + 12.2'+' = '+ (0.5 + 12.2));
+console.log('5 + 1035' + ' = '+ (5 + 1035));
+var numberFor = 4;
+	numberTen = 10;
+	sumOftwoNumbers =  numberFor + numberTen;
+
+document.write('escribiendo en el html el valo de (sumOftwoNumbers): ' + sumOftwoNumbers);
+//document.write('Ancho de la pantalla: '+screen.width+'px'+ ' y el alto es '+screen.height+'px')
+console.log('Sumando numeros usando variables ejemplo: 4 + 10 = '+sumOftwoNumbers);
+console.log('14.5 + 155.75'+' = '+ (14.5 + 155.75));
 console.log('2 * 2' + ' = '+ 2 * 2);
-console.log('11 - 9' + ' = '+ (11 - 9));
-console.log('9 - 11' + ' = '+ (9 - 11));
+console.log('11 - 9' + ' = '+ (25 - 13));
+console.log('9 - 11' + ' = '+ (13 - 25));
 console.log('101.5 - 14.2'+' = '+ (101.5 - 14.2));
 console.log('110 / 4'+' = '+ 110 / 4);
-console.log('310 / 15.5'+' = '+ 310 / 15.5);
+console.log('455 / 16.33'+' = '+ 455 / 16.33);
+console.log('(74 + 25)* 5 / 2 = '+(74 + 25)* 5 / 2 + ' Primero hace la division luego lo multiplica con suma');
 console.log('55 % 20'+' = '+ 55 % 20);
+console.log('554 % 72.5 = '+ 554 % 72.5);
 console.log('sacando el 30 % de 80'+' = '+ 80 * 30 / 100);
 console.log('-----------------usando Objeto Math-----------------');
 console.log('valor de PI es: '+ Math.PI);
