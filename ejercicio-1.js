e08916ff65be81e029f22bc6d6050a41135198e0
Ronny Cabrera <ronny.cabrera@ec.pe>
2015-02-09 15:47:32 -0500

    feat(ejercicios): agregando el primer ejercicio

---
+++ ejercicio-1.js
@@ -0,0 +1,16 @@
+var counterDesc;
+for (counterDesc = 1300; counterDesc > 50; counterDesc--) { 
+	//var counterDsc = counterDesc% 2==1;
+	if (counterDesc % 2 == 1){
+		//si la variable entre 2 deja un residuo 1 (1 = a impar y cero a par).
+		//osea si el residuo de la division es 1 es impar y si el residio de la division es 0 es par
+		// 1300 entre 2 = 650 y no deja residuo osea  el residuo es 0
+		// 1299 entre 2 = 649 deja como residuo 1
+		// ...
+		// ..
+  		//console.log(counterDesc % 2+ " somos impares");
+  		//console.log(counterDesc +" soy impar");
+  		console.log(counterDesc);
+  		document.write(counterDesc + '<br>');
+  	}
+}
--- funciones.js
+++ funciones.js
@@ -8,3 +8,77 @@ function sumTwoNumbers(a, b, c) {
 };
 console.log(sumTwoNumbers(5, 6, 4));
 console.log(sumTwoNumbers(4, 3, 3, 3) + ' ignora el 4to numero');
+console.log('-----------Tipos-----------');
+typeof undefined
+// "undefined"
+typeof null
+// "object"
+typeof true
+// "boolean"
+typeof false
+// "boolean"
+typeof "hola"
+// "string"
+typeof 10
+// "number"
+typeof 1.6
+// "number"
+typeof {}
+// "object"
+console.log('-----------Objetos-----------');
+var object = {};
+var array = [];
+var date = new Date();
+var regexp = /(.*)/;
+
+function func() {};
+
+var string = "";
+var number = 10;
+var bool = true;
+
+object instanceof Object;
+// true
+array instanceof Array;
+// true
+date instanceof Date;
+// true
+regexp instanceof RegExp;
+// true
+func instanceof Function;
+// true
+
+string instanceof String;
+// false
+number instanceof Number;
+// false
+bool instanceof Boolean;
+// false
+
+
+/*
+Valor primitivo	Constructor
+string				String
+number				Number
+Boolean				Boolean*/
+
+console.log('-----------For-----------');
+var counter;
+
+for (counter = 0; counter < 20; counter++) { // inicio del bucle; condición; expresión incremental
+  console.log(counter);
+}
+console.log('-----------Declarando una funcion utilizando los métodos call y apply.-----------');
+function buildSiteTitle(part1, part2) {
+  return part1 + ' — ' + part2;
+};
+var nameStore = 'Lookuma';
+
+console.log(buildSiteTitle(nameStore , 'Catalogo'));
+/* Investigar porque se pasa el null a los metodos call y apply*/
+console.log(buildSiteTitle.call(null, nameStore , 'Las Tiendas'));
+
+console.log(buildSiteTitle.apply(null, [nameStore , 'La Historia']));
+
+
+
